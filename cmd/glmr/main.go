408e2dc9a436dd6999dc40914ca38476d1d4a8d8
apage <apage@mintel.com>
2020-10-15 20:57:05 -0500

    stuff

--- cmd/glmr/main.go
+++ cmd/glmr/main.go
@@ -4,8 +4,13 @@ import (
 	"fmt"
 
 	"gitlab.com/mintel/personal-dev/apage/glmr/stuff"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetDefault("Key", "value")
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yaml")
+	viper.SetConfigName(".glmr")
 	fmt.Printf("Hello, world %d\n", stuff.MyThing(1, 1))
 }
--- go.mod
+++ go.mod
@@ -3,10 +3,6 @@ module gitlab.com/mintel/personal-dev/apage/glmr
 go 1.15
 
 require (
-	github.com/magiconair/properties v1.8.1
-	github.com/mitchellh/go-homedir v1.1.0
-	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.3.0
-	gopkg.in/ini.v1 v1.61.0
 )
