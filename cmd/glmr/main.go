f3b48ba921bd9c297ac34bc5890b3f47099a0b0a
apage <apage@mintel.com>
2020-10-15 21:03:01 -0500

    stuff again

--- cmd/glmr/main.go
+++ cmd/glmr/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 
-	"gitlab.com/mintel/personal-dev/apage/glmr/stuff"
 	"github.com/spf13/viper"
+	"gitlab.com/mintel/personal-dev/apage/glmr/stuff"
 )
 
 func main() {
@@ -12,5 +12,6 @@ func main() {
 	viper.AddConfigPath(".")
 	viper.SetConfigType("yaml")
 	viper.SetConfigName(".glmr")
+
 	fmt.Printf("Hello, world %d\n", stuff.MyThing(1, 1))
 }
